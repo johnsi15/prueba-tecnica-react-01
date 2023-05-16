811be4e1ef4eb1479364662f9b852a2ec6b9caee
John Serrano <andrey_201015@hotmail.com>
2023-05-16 14:34:29 -0500

    Add firts iteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+const URL_API_FACT = 'https://catfact.ninja/fact'
+const URL_API_CAT = 'https://cataas.com/'
 
 function App () {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState(null)
+  const [cat, setCat] = useState(null)
+
+  useEffect(() => {
+    fetch(URL_API_FACT)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        const firstLetter = data.fact.split(' ')[0]
+        console.log(firstLetter)
+
+        setFact(firstLetter)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    fetch(`${URL_API_CAT}/cat/says/${fact}?json=true`)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data)
+        setCat(data.url)
+      })
+  }, [fact])
 
   return (
     <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank' rel="noreferrer">
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank' rel="noreferrer">
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello world</h1>
+      <main>
+        {fact && <h2>This is a cat fact: {fact}</h2>}
+        {cat && <img src={`${URL_API_CAT}/${cat}`} alt={`cat - ${fact}`} />}
+      </main>
     </>
   )
 }
