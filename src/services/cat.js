777dbb072929f9be4159aa0b1ef6b52f17777c32
John Serrano <andrey_201015@hotmail.com>
2023-05-17 16:23:54 -0500

    Add error handler services API

--- src/services/cat.js
+++ src/services/cat.js
@@ -2,11 +2,16 @@ const URL_API_CAT = 'https://cataas.com'
 
 export function getImageCat ({ fact }) {
   return fetch(`${URL_API_CAT}/cat/says/${fact}?json=true`)
-    .then(data => data.json())
+    .then(res => {
+      if (!res.ok) throw new Error('Error  fetching cat image')
+      return res.json()
+    })
     .then(data => {
-      console.log(data)
-      // setCat(data.url)
+      // console.log({ data })
       return data.url
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      console.log(err)
+      return err
+    })
 }
--- src/services/fact.js
+++ src/services/fact.js
@@ -1,9 +1,15 @@
 const URL_API_FACT = 'https://catfact.ninja/fact'
 export function getRandomFact () {
   return fetch(URL_API_FACT)
-    .then(data => data.json())
+    .then(res => {
+      if (!res.ok) throw new Error('Error fetching fact')
+      return res.json()
+    })
     .then(data => {
       return data.fact
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      console.log(err)
+      return err
+    })
 }
