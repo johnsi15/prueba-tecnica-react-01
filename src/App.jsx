a14439c085a81275ea8b0f9b720759d42cb71118
John Serrano <andrey_201015@hotmail.com>
2023-05-18 14:51:01 -0500

    Add test click get new fact

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App () {
     <>
       <h1>App the cats with the first 5 words</h1>
       <main>
-        <button onClick={handleClick}>Get new fact</button>
+        <button className='button-get-fact' onClick={handleClick}>Get new fact</button>
         {fact && <h2 className='fact'>This is a cat fact: {fact}</h2>}
         {imageUrl && <img src={imageUrl} alt={`cat - ${fact}`} />}
       </main>
--- tests/example.spec.js
+++ tests/example.spec.js
@@ -16,3 +16,30 @@ test('app shows random fact and image', async ({ page }) => {
   await expect(textContent?.length).toBeGreaterThan(0)
   await expect(imageSrc?.startsWith(URL_API_CAT)).toBeTruthy()
 })
+
+test('click get new fact button and app shows new random fact and image', async ({ page }) => {
+  await page.goto(LOCALHOST_URL)
+
+  const text = await page.locator('.fact')
+  const image = await page.getByRole('img')
+
+  const textContent = await text.textContent()
+  const imageSrc = await image.getAttribute('src')
+
+  // await page.getByRole('button').click()
+  await page.locator('.button-get-fact').click()
+  // await page.waitForLoadState()
+  await page.waitForTimeout(900)
+
+  const newTextContent = await text.textContent()
+  const newImageSrc = await image.getAttribute('src')
+
+  // console.log({ textContent })
+  // console.log({ newTextContent })
+
+  await expect(textContent).not.toEqual(newTextContent)
+  await expect(imageSrc).not.toEqual(newImageSrc)
+
+  await expect(newTextContent?.length).toBeGreaterThan(0)
+  await expect(newImageSrc?.startsWith(URL_API_CAT)).toBeTruthy()
+})
